ef6d688992a9370d4f77deb15f92a182b4910674
sahanes <sahanes_2000@yahoo.com>
2017-05-10 22:18:14 -0400

    First Commit

--- TestClass2/Class1.cs
+++ TestClass2/Class1.cs
@@ -6,7 +6,86 @@ using System.Threading.Tasks;
 
 namespace TestClass2
 {
-    public class Class1
+    public abstract class Shape
     {
+        public virtual double Area { get; protected set; } = 0;
+        public virtual double Perimeter { get; protected set; } = 0;
+
+        public Shape() { }
+
     }
+
+    public class Square : Shape
+    {
+        public double Width
+        { get { return Width; }
+          set
+            {
+                Width = value;
+                calcProperties(Width);
+            }
+        }
+
+        public Square() { }
+        public Square(double width)
+        {
+            Width = width;
+            calcProperties(Width);
+        }
+
+        private void area(double width)
+        {
+            Area = width * width;
+        }
+
+        private void perimeter(double width)
+        {
+            Perimeter = 4 * width;
+        }
+
+        private void calcProperties(double width)
+        {
+            area(width);
+            perimeter(width);
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius
+        {
+            get { return Radius; }
+            set
+            {
+                Radius = value;
+                calcProperties(Radius);
+            }
+        }
+
+        public Circle() { }
+        public Circle(double radius)
+        {
+            Radius = radius;
+            calcProperties(Radius);
+        }
+
+        private void area(double radius)
+        {
+            Area = Math.Pow(radius,2)*Math.PI;
+        }
+
+        private void perimeter(double radius)
+        {
+            Perimeter = 2*radius*Math.PI;
+        }
+
+        private void calcProperties(double radius)
+        {
+            area(radius);
+            perimeter(radius);
+        }
+
+
+    }
+
 }
